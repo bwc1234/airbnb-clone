cdbb511324cce942d3d465d44a5461b46271030e
WONCHAN BAE <baewonchan1@naver.com>
2021-07-27 16:34:25 +0900

    Get room info from explore and saved screen

--- components/RoomCard.js
+++ components/RoomCard.js
@@ -94,12 +94,14 @@ function getIconName(isFav) {
   }
 }
 
-const RoomCard = ({ id, isFav, isSuperhost, photos, name, price }) => {
+const RoomCard = ({ id, isFav, isSuperhost, photos, name, price, roomObj }) => {
   const dispatch = useDispatch();
   const navigation = useNavigation();
 
   return (
-    <TouchableOpacity onPress={() => navigation.navigate("RoomDetail")}>
+    <TouchableOpacity
+      onPress={() => navigation.navigate("RoomDetail", { ...roomObj })}
+    >
       <Container>
         <TOpacity onPress={() => dispatch(toggleFav(id))}>
           <FavButton>
@@ -157,6 +159,7 @@ RoomCard.propTypes = {
   ),
   name: Pt.string.isRequired,
   price: Pt.number.isRequired,
+  roomObj: Pt.object.isRequired,
 };
 
 export default RoomCard;
--- screens/Main/Room.js
+++ screens/Main/Room.js
@@ -1,5 +1,19 @@
 import React from "react";
+import styled from "styled-components";
 
-export default function Room() {
-  return null;
-}
+const Container = styled.View`
+  flex: 1;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Text = styled.Text``;
+
+export default ({ route: { params } }) => {
+  console.log(params);
+  return (
+    <Container>
+      <Text>Room</Text>
+    </Container>
+  );
+};
