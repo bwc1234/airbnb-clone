84555333f95af93ce643c3f5459820eef121ff76
WONCHAN BAE <baewonchan1@naver.com>
2021-07-31 17:44:52 +0900

    Add map screen

--- api.js
+++ api.js
@@ -5,7 +5,7 @@ const callApi = async (method, path, data, jwt, params = {}) => {
     Authorization: `Bearer ${jwt}`,
     "Content-Type": "application/json",
   };
-  const baseUrl = "http://0bcc69ef7bf5.ngrok.io/api/v1";
+  const baseUrl = "http://3c7452b3eb00.ngrok.io/api/v1";
   const fullUrl = `${baseUrl}${path}`;
   if (method === "get" || method === "delete") {
     return axios[method](fullUrl, { headers, params });
